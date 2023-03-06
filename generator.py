3547021a1e9a69af97ca17509a25adb4a8f4ec06
meikse <meik_seedorf@web.de>
2023-03-06 14:34:17 +0100

    piped input through plant

--- generator.py
+++ generator.py
@@ -2,45 +2,50 @@
 
 from control import tf
 import control
+
 import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(16,6))
 
 from util import *
 
-from csv import reader
+## generate random input data
 
-data = []
-with open("input.csv","r") as file:
-    csv_reader = reader(file, delimiter=",")
-    for i in csv_reader:
-        i = [float(i) for i in i]
-        data.append(i)
+input = InputGenerator(40)
+input.subdivide(8)
+input.gaussian(3)
 
-# print(data)
+input_data = input.get_data()
 
-y = [i[1] for i in data]
-x = [i[0] for i in data]
-# plt.plot(x,y,'x')
+# since all data points must be equally spaced only y data can be used
+# export_csv(input_data) 
+
+u = [i[1] for i in input_data]
+T = [i for i in range(len(u))]
+
+## define plant
 
 # format: s^n ... + s + 1
 nom=[1]
-den=[1,1,1]
+den=[24,4,1]
 g = tf(nom, den)
 
-n = len(data)
-T = [20*i/n for i in range(n)]
-
-from random import random
-U = [random() for _ in range(len(T))]     # noise
-
-# import math
-# U = [math.sin(i) for i in T]              # sin
+## step response
 
 U = [1 for _ in T]                        # step
-
 plt.plot(T,U, '--')
-plt.plot(T,y, '--')
 T,x = control.forced_response(g,T=T, U=U)
 plt.plot(T,x)
-T,x = control.forced_response(g,T=T, U=y)
-plt.plot(T,x)
+
+## pipe input data through plant
+
+plt.plot(T,u, '--')
+T,y = control.forced_response(g,T=T, U=u)
+plt.plot(T,y)
 plt.show()
+
+## export data
+
+data=[[T[i],u[i],y[i]] for i in range(len(T))]
+
+# export_csv(data, 'train.csv')
+# export_csv(data, 'test.csv')
--- util.py
+++ util.py
@@ -5,6 +5,107 @@ from csv import DictReader, writer
 import torch
 from torch.nn import Module, Sequential, Linear, Sigmoid
 
+from random import random 
+from csv import writer, reader
+
+class InputGenerator:
+    '''Generator to create (smooth) input data for a potential plant from random
+    data '''
+    
+    def __init__(self, length) -> None:
+        '''init and storafe of random data points. y data is random between 0-1.
+        x data is randomly incrementing. x and y values are stores in "data" 
+        :length: amount of values on the x-axis'''
+        x = 0 
+        self.data = []
+        for _ in range(length):
+            y = random()
+            x += random() * 2
+            self.data.append([x,y])
+
+
+    def get_data(self):
+        '''returns current data list
+        :data: x and y values '''
+        return self.data
+
+
+    def subdivide(self, steps):
+        '''adds dots on a line between two points in a linear fashion. Input is
+        a list with concurrent lists, like [x, y]
+        :steps: integer for amount of splits'''
+        raw = self.data
+        self. data = []
+        for i in range(len(raw)):
+            if i >= len(raw)-1:
+                break
+            else:
+                xp0 = raw[i][0]
+                yp0 = raw[i][1]
+                xp1 = raw[i+1][0]
+                yp1 = raw[i+1][1]
+                
+                xd = xp1-xp0
+                yd = yp1-yp0
+            
+                for _ in range(steps):
+                    xs = xd / steps
+                    ys = yd / steps
+                    self.data.append([xp0, yp0])
+                    xp0 += xs
+                    yp0 += ys
+
+
+    def gaussian(self, sigma):
+        '''1 dimensional filtering of data via gaussian filter
+        :sigma: sigma for filtering'''
+        from scipy.ndimage import gaussian_filter1d
+        y = [i[-1] for i in self.data]
+        smooth = gaussian_filter1d(y, sigma=sigma)
+        self.data = [[self.data[i][0],smooth[i]] for i in range(len(self.data))]
+
+
+    def lowpass(self, Ts, Ti, V):
+        ''' discrete lowpass filter (pt1) 
+        :Ts:  sampling time 
+        :T1:  integrator time constant
+        :V:   gain
+        '''
+        y_old=0
+        storage=[]
+        data = [i[-1] for i in self.data]
+        for u in data:
+        # $y(k) = \frac{T_1}{T_1+T_s}y(k-1) + \frac{T_s}{T_1+T_s}V\cdot u(k)$
+            y = (Ti/(Ti+Ts)) * y_old  + (Ts*V/(Ti+Ts)) * u
+            y_old = y
+            storage.append(y)
+        self.data =[[self.data[i][0],storage[i]] for i in range(len(self.data))]
+
+
+def export_csv(data, name = "input.csv"):
+    ''' exports the generated input "data" into a csv file.
+    :name: name of file
+    :data: data [[x,y],...]
+    '''
+    with open(name,"w", newline="") as file:
+        csvfile = writer(file, delimiter=",")
+        for i in data:
+            csvfile.writerow(i)
+
+
+def import_csv(name = "input.csv"):
+    ''' exports the generated input "data" into a csv file.
+    :name: name of file
+    :data: data [[x,y],...]
+    '''
+    data = []
+    with open(name,"r") as file:
+        csv_reader = reader(file, delimiter=",")
+        for i in csv_reader:
+            i = [float(i) for i in i]
+            data.append(i)
+    return data
+
 
 def loadData(path, file):
     with open((path / file).as_posix()) as file:
