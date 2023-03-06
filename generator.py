fa301274589054e616f68b0a16e862f4b4d57af5
meikse <meik_seedorf@web.de>
2023-03-06 13:23:56 +0100

    adjust generator to input format

--- generator.py
+++ generator.py
@@ -12,11 +12,14 @@ data = []
 with open("input.csv","r") as file:
     csv_reader = reader(file, delimiter=",")
     for i in csv_reader:
+        i = [float(i) for i in i]
         data.append(i)
 
-data = data[0]
-data = [float(i) for i in data]
 # print(data)
+
+y = [i[1] for i in data]
+x = [i[0] for i in data]
+# plt.plot(x,y,'x')
 
 # format: s^n ... + s + 1
 nom=[1]
@@ -26,8 +29,8 @@ g = tf(nom, den)
 n = len(data)
 T = [20*i/n for i in range(n)]
 
-# from random import random
-# U = [random() for _ in range(len(T))]     # noise
+from random import random
+U = [random() for _ in range(len(T))]     # noise
 
 # import math
 # U = [math.sin(i) for i in T]              # sin
@@ -35,9 +38,9 @@ T = [20*i/n for i in range(n)]
 U = [1 for _ in T]                        # step
 
 plt.plot(T,U, '--')
-plt.plot(T,data, '--')
+plt.plot(T,y, '--')
 T,x = control.forced_response(g,T=T, U=U)
 plt.plot(T,x)
-T,x = control.forced_response(g,T=T, U=data)
+T,x = control.forced_response(g,T=T, U=y)
 plt.plot(T,x)
 plt.show()
