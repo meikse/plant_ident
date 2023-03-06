ea4f110975cc0e587af5c369f6515e00eeae5750
meikse <meik_seedorf@web.de>
2023-03-06 12:12:29 +0100

    converted to class

--- input.py
+++ input.py
@@ -1,95 +1,104 @@
 #!/usr/bin/python3
 
 import matplotlib.pyplot as plt
-import math
 from random import random 
-
-# ------------------------------------------------------------------------------
-# generate random data as input
-
-n = 20
-u = [random() for _ in range(n)]     # noise
-
-slope = []
-for i, val in enumerate(u):
-    if i >= len(u)-1:
-        break
-    y = u[i+1]-u[i] # m = y/1, with x = 1
-    slope.append(y)
-
-## 
-
-angle = [abs(round(math.sin(s*math.pi),1))*10 for s in slope]
-
-steps = []
-step = .1
-for n in angle:
-    l = 0
-    L=[]
-    for i in range(int(n)):
-        l += step
-        L.append(round(l,2))
-    steps.append(L)
-
-b = 0
-val = 0
-data = []
-for i,s in enumerate(slope):
-    b = val
-    for t in steps[i]:
-        val = s * t + b
-        data.append(val)
-
-# ------------------------------------------------------------------------------
-# plot 
-
-fig, ax = plt.subplots(figsize=(16,6))
-plt.grid()
-plt.plot([0,120],[0,0],'k')
-plt.plot([0,0],[0,1],'k')
-plt.plot(data, '-o')
-plt.plot( u)
-
-# ------------------------------------------------------------------------------
-# smooth data via "scipy" gaussian filtering
-
-from scipy.ndimage import gaussian_filter1d
-
-smooth = gaussian_filter1d(data, sigma=3)
-smooth = [i-smooth[0] for i in smooth]
-
-plt.plot(smooth)
-
-# ------------------------------------------------------------------------------
-# smooth data via discrete low pass filtering
-
-#init
-Ts=1 # sampling time 
-T1=5 # integrator time constant
-V=1  # gain
-
-#plant 
-# $y(k) = \frac{T_1}{T_1+T_s}y(k-1) + \frac{T_s}{T_1+T_s}V\cdot u(k)$
-def pt1(u, y_old, Ts = 1, V = 1, T1 = 1):
-    return (T1/(T1+Ts)) * y_old  + (Ts*V/(T1+Ts)) * u
-
-#starting condition
-y_old=0
-lowpassed= []
-
-for u in data:
-    y = pt1(u,y_old, Ts, V, T1)
-    y_old = y
-    lowpassed.append(y)
-    
-# show result
-plt.plot(lowpassed)
-
-# ------------------------------------------------------------------------------
-# save data
-
 from csv import writer
 
-with open("input.csv","w", newline="") as file:
-    data = writer(file, delimiter=",")
-    data.writerow(lowpassed)
+class InputGenerator:
+
+    
+    def __init__(self, length) -> None:
+        x = 0 
+        self.data = []
+        for _ in range(length):
+            y = random()
+            x += random() * 2
+            self.data.append([x,y])
+
+
+    def get_data(self):
+        return self.data
+
+
+    def subdivide(self, steps):
+        raw = self.data
+        self. data = []
+        for i in range(len(raw)):
+            if i >= len(raw)-1:
+                break
+            else:
+                xp0 = raw[i][0]
+                yp0 = raw[i][1]
+                xp1 = raw[i+1][0]
+                yp1 = raw[i+1][1]
+                
+                xd = xp1-xp0
+                yd = yp1-yp0
+            
+                for _ in range(steps):
+                    xs = xd / steps
+                    ys = yd / steps
+                    self.data.append([xp0, yp0])
+                    xp0 += xs
+                    yp0 += ys
+
+    def gaussian(self, sigma):
+        from scipy.ndimage import gaussian_filter1d
+        y = [i[-1] for i in self.data]
+        smooth = gaussian_filter1d(y, sigma=sigma)
+        self.data = [[self.data[i][0],smooth[i]] for i in range(len(self.data))]
+
+
+    def lowpass(self, Ts, Ti, V):
+        # $y(k) = \frac{T_1}{T_1+T_s}y(k-1) + \frac{T_s}{T_1+T_s}V\cdot u(k)$
+        #init
+        # Ts=1 # sampling time 
+        # T1=5 # integrator time constant
+        # V=1  # gain
+        #starting condition
+        y_old=0                                 # origin at 0
+        storage=[]
+        data = [i[-1] for i in self.data]
+        for u in data:
+            y = (Ti/(Ti+Ts)) * y_old  + (Ts*V/(Ti+Ts)) * u
+            y_old = y
+            storage.append(y)
+        self.data =[[self.data[i][0],storage[i]] for i in range(len(self.data))]
+
+    def export(self, name = "input.csv"):
+        with open(name,"w", newline="") as file:
+            csvfile = writer(file, delimiter=",")
+            for i in self.data:
+                csvfile.writerow(i)
+
+
+test = InputGenerator(20)
+fig, ax = plt.subplots(figsize=(16,6))
+
+# data = test.get_data()
+# y = [i[1] for i in data]
+# x = [i[0] for i in data]
+# plt.plot(x,y)
+
+test.subdivide(8)
+
+# data = test.get_data()
+# y = [i[1] for i in data]
+# x = [i[0] for i in data]
+# plt.plot(x,y,'o')
+
+test.gaussian(3)
+
+# data = test.get_data()
+# y = [i[1] for i in data]
+# x = [i[0] for i in data]
+# plt.plot(x,y,'o')
+
+# test.lowpass(1,5,1)
+
+data = test.get_data()
+y = [i[1] for i in data]
+x = [i[0] for i in data]
+plt.plot(x,y,'x')
+
+test.export()
