f326bf993cc475f32d4661b9cc62501ad187479e
meikse <meik_seedorf@web.de>
2023-03-19 22:03:23 +0100

    changed tf to ss

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from control import tf, forced_response
+from control import tf, ss, forced_response
 from util import InputGenerator, export_csv
 
 
@@ -14,13 +14,21 @@ def main():
         u = [i[1] for i in input_data]
         T = [i for i in range(len(u))]
     
-        # format: s^n ... + s + 1
-        nom=[1]
-        den=[24,4,1]
-        g = tf(nom, den)
-    
+        # # format: s^n ... + s + 1
+        # nom=[1]
+        # den=[24,4,1]
+        # g = tf(nom, den)
+
+        # choose a stable one, this one is unstable ! TODO
+        a = [[-1/6, -1/24],[1, 0]]
+        b = [[1],[0]]
+        c = [1, 0]
+        # c = [0, 1]
+        # c = [0, 1/24]
+        g = ss(a,b,c,0)
+
         T,y = forced_response(g,T=T, U=u)
-    
+
         output_data=[[T[i],u[i],y[i]] for i in range(len(T))]
         export_csv(output_data, "{}.csv".format(name))
         
