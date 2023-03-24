first working pipeline with wrong results

main.py
@@ -1,60 +1,162 @@
 #!/usr/bin/python3
 
 from util import *
-
 from pathlib import Path
 
 from torch import tensor
-from torch.utils.data import DataLoader, TensorDataset
-from torch.optim import Adam 
+
+import matplotlib.pyplot as plt
+
+import logging
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 ## load data
 
 PATH = Path(".")
-FILENAME = "data.csv"
-data = loadData(PATH,FILENAME)
+FILENAME = "train.csv"
+name = (PATH / FILENAME).as_posix()
+train_data = import_csv(name = name)
+
+FILENAME = "test.csv"
+name = (PATH / FILENAME).as_posix()
+test_data = import_csv(name = name)
 
 ## map dict -> tensor
-us, ys, ut, yt = map(tensor,(data['uEst'], # train input
-                             data['uVal'], # train output
-                             data['yEst'], # test  input
-                             data['yVal']))# test  outputtensors 
 
-## init dataset/dataloader
-# training dataset
-train_ds = TensorDataset((us),(ys)) 
-train_dl = DataLoader(train_ds)
-# test dataset
-test_ds = TensorDataset((ut),(ut)) 
-test_dl = DataLoader(test_ds)
+# T = tensor([i[0] for i in train_data])  # sample data
+us_raw = tensor([i[1] for i in train_data])
+ys_raw = tensor([i[2] for i in train_data])
+ut_raw = tensor([i[1] for i in test_data] )
+yt_raw = tensor([i[2] for i in test_data] )
+
+## plot data
+
+fig1, ax1 = plt.subplots(figsize=(16,8))
+plt.plot(us_raw, label="input  (train)")
+plt.plot(ys_raw, label="output (train)")
+plt.legend()
+plt.close(fig1)
+
+fig2, ax2 = plt.subplots(figsize=(16,8))
+plt.plot(ut_raw, label="input  (test)")
+plt.plot(yt_raw, label="output (test)")
+plt.legend()
+plt.close(fig2)
+
+## prepare data
+# no offset from mean value 
+us = us_raw / us_raw.std(unbiased=False)
+ys = ys_raw / ys_raw.std(unbiased=False)
+ut = ut_raw / ut_raw.std(unbiased=False) 
+yt = yt_raw / yt_raw.std(unbiased=False)
+us = us.unsqueeze(dim=0).t()
+ys = ys.unsqueeze(dim=0).t()
+ut = ut.unsqueeze(dim=0).t()
+yt = yt.unsqueeze(dim=0).t()
+
+## plot data
+
+fig3, ax3 = plt.subplots(figsize=(16,8))
+plt.plot(us_raw, label="raw")
+plt.plot(us, label="cleaned")
+plt.legend()
+plt.close(fig1)
+
+fig4, ax4 = plt.subplots(figsize=(16,8))
+plt.plot(ys_raw, label="raw")
+plt.plot(ys, label="cleaned")
+plt.legend()
+plt.close(fig2)
+
 ## init model
-# instantiate model
-model = Logistic()
 
-def fit(loader, net, loss, num_epochs=1):
-    opt = Adam(net.parameters(), lr=0.01)
+n_x = 2
+n_u = 1
+model = Model(n_x=n_x, n_u=n_u)
 
-    # empty lists for storage
-    losses = []
-    epochs = []
+## prepare input vector
 
-    
-    for epoch in range(num_epochs):
-        print(f'Epoch {epoch} --> ', end="\t")
-        N = len(loader)
-        for i, (x, y) in enumerate(loader):
-            # Update the weights of the network
-            opt.zero_grad()               # reset gradients 
+# create empty (zero) matrix for possible states
+X=torch.zeros((len(train_data),n_x))
+# map output column of the train data to first column of the state matrix.
+# since x1 = y, means C = [1 0]. just control theory things
+X[:,0]=ys[:,0]
+# establish gradient descent computable state matrix
+X_est=X.requires_grad_()
+# extract first row of the states matrix
+X0=X_est[0,:]
 
-            loss_value = loss(net(x), y)  # forwarding
+steps = 20000
+lr = 1e-5
+dt = 1 # Ts
 
-            loss_value.backward()         # backwarding
-            opt.step() 
-            # Store training data
-            epochs.append(epoch+i/N)
-            losses.append(loss_value.item())
-        # print('loss: {:.2}'.format(loss_value.item()), end="\n") # wrong just the last
-    return epochs, losses
+## init optimizer
+
+from torch import optim
+
+params_net = list(model.parameters())
+optimizer = optim.Adam(params_net, lr = lr)
 
 ## fit model
-# fit(train_ds, model, costFunc, 2)
+
+for i in range(1, steps + 1):
+    # set back gradient for every iteration step
+    optimizer.zero_grad()
+    
+    # perform one-step ahead prediction
+    # pipe N_f net through the integrator
+    xdot_int= pipe(model, X_est, us, dt)
+    
+    # Compute fit loss
+    error  = xdot_int - X_est
+    loss = torch.mean(error**2)*dt
+
+    if not i % 10:
+        logging.info(round(loss.item(),2))
+        logging.info(i)
+    
+    # optimization step
+    loss.backward()
+    # next step 
+    optimizer.step()
+
+
+## evaluate
+
+# predict states with model 
+X_sim=evaluate(model,X0,n_x,us,dt)     # here us
+# last state is also output
+y_ss=X_sim[:,0]
+y_ss=y_ss.unsqueeze(dim=0).t()
+# add mean error and multiplay by standard output
+y_sim=(y_ss*ys.std(unbiased=False))+ys.mean()
+# calculate RMS of the resulting output 
+# RMS_train=RMSE(y_sim,ys_raw)
+
+# predict states with model 
+X_sim_val=evaluate(model,X0,n_x,ut,dt) # here ut
+# last state is also output
+y_ss_val=X_sim_val[:,0]
+y_ss_val=y_ss_val.unsqueeze(dim=0).t()
+# add mean error and multiplay by standard output
+y_sim_val=(y_ss_val*ys.std(unbiased=False))+ys.mean()
+# calculate RMS of the resulting output 
+# RMS_test=RMSE(y_sim_val,yt_raw)
+
+## plot results
+
+# show train results
+figt, axt = plt.subplots(figsize=(16,8))
+plt.plot(y_sim.squeeze().tolist(), 'b', label='Output sim')
+plt.plot(ys_raw, 'k', label='Output ')
+plt.title('Train Data')
+plt.legend();
+
+# show test results
+figv, axv = plt.subplots(figsize=(16,8))
+plt.plot(y_sim_val.squeeze().tolist(), 'b', label='Output sim')
+plt.plot(yt_raw, 'k', label='Output ')
+plt.title('Test Data')
+plt.legend();
+
+# plt.show()

util.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python3
 
-from csv import DictReader, writer
-
 import torch
 from torch.nn import Module, Sequential, Linear, Sigmoid
 
 from random import random 
 from csv import writer, reader
+
+
+def export_csv(data, name = "input.csv"):
+    ''' exports the generated input "data" into a csv file.
+    :name: name of file
+    :data: data [[x,y],...]
+    '''
+    with open(name,"w", newline="") as file:
+        csvfile = writer(file, delimiter=",")
+        for i in data:
+            csvfile.writerow(i)
+
+
+def import_csv(name = "input.csv"):
+    ''' exports the generated input "data" into a csv file.
+    :name: name of file
+    :data: data [[x,y],...]
+    '''
+    data = []
+    with open(name,"r") as file:
+        csv_reader = reader(file, delimiter=",")
+        for i in csv_reader:
+            i = [float(i) for i in i]
+            data.append(i)
+    return data
+
 
 class InputGenerator:
     '''Generator to create (smooth) input data for a potential plant from random
@@ -82,104 +106,91 @@ class InputGenerator:
         self.data =[[self.data[i][0],storage[i]] for i in range(len(self.data))]
 
 
-def export_csv(data, name = "input.csv"):
-    ''' exports the generated input "data" into a csv file.
-    :name: name of file
-    :data: data [[x,y],...]
-    '''
-    with open(name,"w", newline="") as file:
-        csvfile = writer(file, delimiter=",")
-        for i in data:
-            csvfile.writerow(i)
-
-
-def import_csv(name = "input.csv"):
-    ''' exports the generated input "data" into a csv file.
-    :name: name of file
-    :data: data [[x,y],...]
-    '''
-    data = []
-    with open(name,"r") as file:
-        csv_reader = reader(file, delimiter=",")
-        for i in csv_reader:
-            i = [float(i) for i in i]
-            data.append(i)
-    return data
-
-# -----------------------------------------------------------------------------
-
-from torch.nn import Module, Sequential, Linear, Sigmoid
-
-class Logistic(Module):
+class Model(Module):
    
-    def __init__(self, n_x, n_u, n_feat=[40,40,40]):
+    def __init__(self, n_x, n_u):
         '''
         n_u: number of inputs
         n_x: number of states 
         n_fest: number of neurons per layer of the state mapping function
         '''
-        super(Logistic, self).__init__()
-        self.net = Sequential(
-            Linear(n_x + n_u, n_feat[0]),  # 2 states, 1 input
-            Sigmoid(),
-            Linear(n_feat[0], n_feat[1]),
-            Sigmoid(),
-            Linear(n_feat[1], n_feat[2]),
-            Sigmoid(),
-            Linear(n_feat[2], n_x),         # n_x since states=output
-            )
+        super(Model, self).__init__()
+        n = 40
+        self.net = Sequential(Linear(n_x + n_u, n),  
+                              Sigmoid(),
+                              Linear(n,n),
+                              Sigmoid(),
+                              Linear(n,n),
+                              Sigmoid(),
+                              Linear(n, n_x),        # n_x since states=output
+                              )
     
+
     def forward(self, X,U):
-        '''is not called directly, optimizer is calling it implicitly'''
+        '''is not called directly, optimizer is calling it implicitly
+        :X: state values in form of a Matrix
+        :U: input values in form of a Matrix
+        :return: evaluation of the net
+        '''
         # concatenate inputs and stated for passing them once into the net
         XU = torch.cat((X,U),-1)
-        # i guess it does not matter passing them concatenated into the net
-        DX = self.net(XU)
+        # it does not matter passing them concatenated into the net
+        return self.net(XU)
 
-        return DX
-    
 
-class INN:
-    
-    def __init__(self, nn_model):
-        # pass net to this class
-        self.nn_model = nn_model
+def pipe(model, X, U, dt):
+    ''' pipe for the block diagram:
 
-    def INN_est(self, X_est,U,dt):
+        x_dot_int = x_0 + int(N_f(x,u),dt)
+        x = x_dot_int       # next iteration step
 
-        # pass u and x_head into N_f (Fig. 1) dot_x_head 
-        X_est_torch = self.nn_model(X_est.float(),U.float())
-        # integrate via cumulative sum of all previous estimated x_i
-        X_sum = torch.cumsum(X_est_torch, dim=0) # integrator
-        # first row will be extracted
-        # x0=X_est[0,:]
-        x0=X_est[0]
-        # adding initial state -> x_0
-        xdot_int=torch.add(x0,dt*X_sum)
+    :model: neural network
+    :X      states
+    :U:     initial state, X[0]
+    :dt:    time constant (integer)
 
-        return xdot_int
+    returns integrated states provided by the neural network
+    '''
+    # pass u and x_head into N_f (Fig. 1) dot_x_head 
+    xhead_i = model(X.float(),U.float())
+    # integrate via cumulative sum of all previous estimated x_i
+    X_sum = torch.cumsum(xhead_i, dim=0) # integrator
+    # adding initial state -> x0
+    xdot_int=torch.add(X[0,:], dt*X_sum)
 
-# def fit(loader, net, loss, num_epochs=1):
-#     opt = Adam(net.parameters(), lr=0.01)
+    return xdot_int
 
-#     # empty lists for storage
-#     losses = []
-#     epochs = []
 
-    
-#     for epoch in range(num_epochs):
-#         print(f'Epoch {epoch} --> ', end="\t")
-#         N = len(loader)
-#         for i, (x, y) in enumerate(loader):
-#             # Update the weights of the network
-#             opt.zero_grad()               # reset gradients 
+def evaluate(model, X0, n_x, U, dt):
+    ''' evaluate trained model discretely
 
-#             loss_value = loss(net(x.unsqueeze(dim=0)), y)  # forwarding
+    :X0:  row vector of initial states
+    :n_x: number of states 
+    :us:  is a vector of inputs
+    :dt:  size of time step 
 
-#             loss_value.backward()         # backwarding
-#             opt.step() 
-#             # Store training data
-#             epochs.append(epoch+i/N)
-#             losses.append(loss_value.item())
-#         # print('loss: {:.2}'.format(loss_value.item()), end="\n") # wrong just the last
-#     return epochs, losses
+    returns output from the feeded neural network + integrator
+    '''
+    X_list=X0
+    X_sim=torch.zeros((U.shape[0],n_x))          # matrix 
+    x_sum=X_list                                 # vector
+    for i, val in enumerate(U):
+        # previous state, new input 
+        x_p1 = model(X_list.float(),val.float()) # vector
+        # time constant * current state + previous output
+        x_sum = dt * x_p1 + x_sum
+        # update initial state for the next loop
+        X_list=x_sum
+        # save state in matrix
+        X_sim[i,:]=X_list
+    return X_sim
+
+
+def RMSE(y_sim, y_val):
+    ''' root-mean-square error
+    :y_sim:  simulated vector
+    :y_val:  comparison vector
+    '''
+    error = y_sim - y_val
+    N = y_sim.shape[0]
+    return torch.sqrt(((1/N)*torch.sum(error**2)))