c912161106644443e734e87c44121aae214796d9
meikse <meik_seedorf@web.de>
2023-03-28 23:27:36 +0200

    included raw data into data dict

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from torch import tensor
 # from torch.utils.data import TensorDataset, DataLoader
 
 import matplotlib.pyplot as plt; color = "red"
+plt.style.use('dark_background')
+
 import logging
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
@@ -17,20 +19,20 @@ train_data = import_csv(path = path, name = "train.csv")
 test_data = import_csv(path = path, name = "test.csv")
 
 # assign systems: input, output, and timestamp
-train_raw = {param : tensor([i[n] for i in train_data[1:]]) 
+train = {param : tensor([i[n] for i in train_data[1:]]) 
          for n, param in enumerate(train_data[0][:3])}
-test_raw = {param : tensor([i[n] for i in test_data[1:]]) 
+test = {param : tensor([i[n] for i in test_data[1:]]) 
         for n, param in enumerate(test_data[0][:3])}
 
 ## plot data
 
 fig1, ax1 = plt.subplots(2,1,figsize=(16,12))
 ax1[0].set_title("train data", color=color)
-ax1[0].plot(train_raw["T"],train_raw["u"], label="u (raw)")
-ax1[0].plot(train_raw["T"],train_raw["y"], label="y (raw)")
+ax1[0].plot(train["T"],train["u"], label="u (raw)")
+ax1[0].plot(train["T"],train["y"], label="y (raw)")
 ax1[1].set_title("test data", color=color)
-ax1[1].plot(test_raw["T"],test_raw["u"], label="u (raw)")
-ax1[1].plot(test_raw["T"],test_raw["y"], label="y (raw)")
+ax1[1].plot(test["T"],test["u"], label="u (raw)")
+ax1[1].plot(test["T"],test["y"], label="y (raw)")
 for ax in ax1:
     ax.legend()
     ax.grid()
@@ -40,18 +42,20 @@ plt.close(fig1)
 ## normalize data
 
 # extract "T" from it since it lies on the abzyss. only "u" and "y"
-train_uy = {key: train_raw[key] for key in ["u", "y"]}
-test_uy = {key: test_raw[key] for key in ["u", "y"]}
+train_uy = {key: train[key] for key in ["u", "y"]}
+test_uy = {key: test[key] for key in ["u", "y"]}
 # merging normalized data in new dict with old one containing "T"
-train= train_raw | dict(map(lambda d: (d[0], normalize(d[1])), train_uy.items()))
-test = test_raw | dict(map(lambda d: (d[0], normalize(d[1])), test_uy.items()))
+train= train | dict(map(lambda d: (d[0] + "_norm", normalize(d[1])),
+                        train_uy.items()))
+test = test | dict(map(lambda d: (d[0] + "_norm", normalize(d[1])),
+                        test_uy.items()))
 
 ## plot data
 
-ax1[0].plot(train["T"],train["u"], label="u (norm)")
-ax1[0].plot(train["T"],train["y"], label="y (norm)")
-ax1[1].plot(test["T"],test["u"], label="u (norm)")
-ax1[1].plot(test["T"],test["y"], label="y (norm)")
+ax1[0].plot(train["T"],train["u_norm"], label="u (norm)")
+ax1[0].plot(train["T"],train["y_norm"], label="y (norm)")
+ax1[1].plot(test["T"],test["u_norm"], label="u (norm)")
+ax1[1].plot(test["T"],test["y_norm"], label="y (norm)")
 for ax in ax1:
     ax.legend()
 
@@ -65,10 +69,10 @@ model = Model(n_x=nx, n_u=nu)
 ## prepare data
 
 # create empty (zero) matrix for possible states
-X=torch.zeros((len(train["y"]),nx))
+X=torch.zeros((len(train["y_norm"]),nx))
 # map output column of the train data to first column of the state matrix.
 # since x1 = y, means C = [1 0]. just control theory things
-X[:,0]=train["y"].squeeze()
+X[:,0]=train["y_norm"].squeeze()
 X.requires_grad_()
 
 # extract first row of the states matrix
@@ -81,8 +85,8 @@ X0=X[0,:]
 
 ## init optimizer
 
-steps = 40000
-lr = 1e-5
+steps = 10000
+lr = 1e-4
 dt = 4 # Ts
 
 from torch import optim
@@ -99,7 +103,7 @@ def fit():
             
             # perform one-step ahead prediction
             # pipe N_f net through the integrator
-            xdot_int= pipe(model, X, train["u"], dt)
+            xdot_int= pipe(model, X, train["u_norm"], dt)
             
             # Compute fit loss
             error  = xdot_int - X
@@ -119,26 +123,25 @@ def fit():
 
 for data in [train, test]:
     # predict states with model 
-    X_sim=evaluate(model,X0,nx,data["u"],dt)
+    X_sim=evaluate(model,X0,nx,data["u_norm"],dt)
     # last state is also output
     y_ss=X_sim[:,0]
     y_ss=y_ss.unsqueeze(dim=0).t()
     # add mean error and multiplay by standard output
     y_sim=denormalize(y_ss)
     # compute error in respect to reference output
-    error = RMSE(y_sim,data["y"])
+    error = RMSE(y_sim,data["y_norm"])
     # store into dicts
     data.update({"y_sim": y_sim, "error": error})
 
 ## plot results
-
 fig2, ax2 = plt.subplots(2,1,figsize=(16,12))
 ax2[0].set_title("train data, RMSe: {:.2}".format(train["error"]), color=color)
-ax2[0].plot(train["y_sim"].squeeze().tolist(), 'b', label='y (sim)')
-ax2[0].plot(train["y"], 'k', label='y (norm)') # why here not train_raw? TODO
+ax2[0].plot(train["y_sim"].detach(), 'r', label='y (sim)')
+ax2[0].plot(train["y_norm"], 'w', label='y (norm)') #why here not train_raw? TODO
 ax2[1].set_title("test data, RMSe: {:.2}".format(test["error"]), color=color)
-ax2[1].plot(test["y_sim"].squeeze().tolist(), 'b', label='y (sim)')
-ax2[1].plot(test["y"], 'k', label='y (norm)')  # why here not test raw ? TODO
+ax2[1].plot(test["y_sim"].detach(), 'r', label='y (sim)')
+ax2[1].plot(test["y_norm"], 'w', label='y (norm)')  #why here not test raw ? TODO
 for ax in ax2:
     ax.legend()
     ax.grid()
